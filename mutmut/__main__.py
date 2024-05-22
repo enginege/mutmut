cf17c6d232341b66be6e0317f85cf23ca1baad6e
Eren Copcu <copcu@sabanciuniv.edu>
2024-05-22 18:54:39 +0200

    refactor: Combined do_run functionality into run to reduce the unnecessary functions

--- mutmut/__main__.py
+++ mutmut/__main__.py
@@ -152,10 +152,66 @@ def run(argument, paths_to_mutate, disable_mutation_types, enable_mutation_types
     if test_time_multiplier is None:  # click sets the default=0.0 to None
         test_time_multiplier = 0.0
 
-    sys.exit(do_run(argument, paths_to_mutate, disable_mutation_types, enable_mutation_types, runner,
-                    tests_dir, test_time_multiplier, test_time_base, swallow_output, use_coverage,
-                    dict_synonyms, pre_mutation, post_mutation, use_patch_file, paths_to_exclude,
-                    simple_output, no_progress, ci, rerun_all, max_workers))
+    mutation_test_runner = MutationTestRunner(Config(
+        total=0,
+        swallow_output=not swallow_output,
+        test_command=runner,
+        covered_lines_by_filename=None,
+        coverage_data={},
+        baseline_time_elapsed=0.0,
+        dict_synonyms=dict_synonyms,
+        using_testmon='--testmon' in runner,
+        tests_dirs=[],
+        hash_of_tests='',
+        test_time_multiplier=test_time_multiplier,
+        test_time_base=test_time_base,
+        pre_mutation=pre_mutation,
+        post_mutation=post_mutation,
+        paths_to_mutate=[],
+        mutation_types_to_apply=set(),
+        no_progress=no_progress,
+        ci=ci,
+        rerun_all=rerun_all
+    ))
+
+    mutation_test_runner.validate_arguments(use_coverage, use_patch_file, disable_mutation_types, enable_mutation_types)
+
+    dict_synonyms = [x.strip() for x in dict_synonyms.split(',')]
+
+    if use_coverage and not exists('.coverage'):
+        raise FileNotFoundError('No .coverage file found. You must generate a coverage file to use this feature.')
+
+    paths_to_mutate = mutation_test_runner.get_paths_to_mutate(paths_to_mutate)
+    tests_dirs = mutation_test_runner.get_tests_dirs(tests_dir)
+
+    current_hash_of_tests = hash_of_tests(tests_dirs)
+
+    mutation_types_to_apply = mutation_test_runner.get_mutation_types_to_apply(disable_mutation_types,
+                                                                               enable_mutation_types)
+
+    covered_lines_by_filename = mutation_test_runner.get_covered_lines_by_filename(use_coverage, use_patch_file)
+    coverage_data = read_coverage_data() if use_coverage else None
+
+    mutation_test_runner.config.covered_lines_by_filename = covered_lines_by_filename
+    mutation_test_runner.config.coverage_data = coverage_data
+    mutation_test_runner.config.tests_dirs = tests_dirs
+    mutation_test_runner.config.hash_of_tests = current_hash_of_tests
+    mutation_test_runner.config.paths_to_mutate = paths_to_mutate
+    mutation_test_runner.config.mutation_types_to_apply = mutation_types_to_apply
+
+    mutation_test_runner.setup_environment()
+    baseline_time_elapsed = mutation_test_runner.run_baseline_tests()
+    mutation_test_runner.config.baseline_time_elapsed = baseline_time_elapsed
+
+    mutations_by_file = mutation_test_runner.generate_mutations(argument, dict_synonyms, paths_to_exclude,
+                                                                paths_to_mutate, tests_dirs)
+
+    print()
+    print('2. Checking mutants')
+    progress = Progress(total=mutation_test_runner.config.total,
+                        output_legend=mutation_test_runner.get_output_legend(simple_output), no_progress=no_progress)
+
+    sys.exit(mutation_test_runner.run_mutation_tests(progress, mutations_by_file, max_workers=max_workers))
 
 
 @climain.command(context_settings=dict(help_option_names=['-h', '--help']))
@@ -250,90 +306,6 @@ def html(dict_synonyms, directory):
     """
     create_html_report(dict_synonyms, directory)
     sys.exit(0)
-
-
-def do_run(
-        argument,
-        paths_to_mutate,
-        disable_mutation_types,
-        enable_mutation_types,
-        runner,
-        tests_dir,
-        test_time_multiplier,
-        test_time_base,
-        swallow_output,
-        use_coverage,
-        dict_synonyms,
-        pre_mutation,
-        post_mutation,
-        use_patch_file,
-        paths_to_exclude,
-        simple_output,
-        no_progress,
-        ci,
-        rerun_all,
-        max_workers,
-) -> int:
-    mutation_test_runner = MutationTestRunner(Config(
-        total=0,
-        swallow_output=not swallow_output,
-        test_command=runner,
-        covered_lines_by_filename=None,
-        coverage_data={},
-        baseline_time_elapsed=0.0,
-        dict_synonyms=dict_synonyms,
-        using_testmon='--testmon' in runner,
-        tests_dirs=[],
-        hash_of_tests='',
-        test_time_multiplier=test_time_multiplier,
-        test_time_base=test_time_base,
-        pre_mutation=pre_mutation,
-        post_mutation=post_mutation,
-        paths_to_mutate=[],
-        mutation_types_to_apply=set(),
-        no_progress=no_progress,
-        ci=ci,
-        rerun_all=rerun_all
-    ))
-
-    mutation_test_runner.validate_arguments(use_coverage, use_patch_file, disable_mutation_types, enable_mutation_types)
-
-    dict_synonyms = [x.strip() for x in dict_synonyms.split(',')]
-
-    if use_coverage and not exists('.coverage'):
-        raise FileNotFoundError('No .coverage file found. You must generate a coverage file to use this feature.')
-
-    paths_to_mutate = mutation_test_runner.get_paths_to_mutate(paths_to_mutate)
-    tests_dirs = mutation_test_runner.get_tests_dirs(tests_dir)
-
-    current_hash_of_tests = hash_of_tests(tests_dirs)
-
-    mutation_types_to_apply = mutation_test_runner.get_mutation_types_to_apply(disable_mutation_types,
-                                                                               enable_mutation_types)
-
-    covered_lines_by_filename = mutation_test_runner.get_covered_lines_by_filename(use_coverage, use_patch_file)
-    coverage_data = read_coverage_data() if use_coverage else None
-
-    mutation_test_runner.config.covered_lines_by_filename = covered_lines_by_filename
-    mutation_test_runner.config.coverage_data = coverage_data
-    mutation_test_runner.config.tests_dirs = tests_dirs
-    mutation_test_runner.config.hash_of_tests = current_hash_of_tests
-    mutation_test_runner.config.paths_to_mutate = paths_to_mutate
-    mutation_test_runner.config.mutation_types_to_apply = mutation_types_to_apply
-
-    mutation_test_runner.setup_environment()
-    baseline_time_elapsed = mutation_test_runner.run_baseline_tests()
-    mutation_test_runner.config.baseline_time_elapsed = baseline_time_elapsed
-
-    mutations_by_file = mutation_test_runner.generate_mutations(argument, dict_synonyms, paths_to_exclude,
-                                                                paths_to_mutate, tests_dirs)
-
-    print()
-    print('2. Checking mutants')
-    progress = Progress(total=mutation_test_runner.config.total,
-                        output_legend=mutation_test_runner.get_output_legend(simple_output), no_progress=no_progress)
-
-    return mutation_test_runner.run_mutation_tests(progress, mutations_by_file, max_workers=max_workers)
 
 
 def parse_run_argument(argument, config, dict_synonyms, mutations_by_file, paths_to_exclude, paths_to_mutate,
